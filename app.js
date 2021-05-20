83b9c74dee9034fca736d3e2b14441c23c78e98e
Aman Tandon <aman.tandon3096@gmail.com>
2021-05-20 13:21:46 +0530

    working with querySelector

---
+++ app.js
@@ -0,0 +1,9 @@
+let inputEle = document.querySelector('#inp');
+console.log(inputEle);
+
+let btnTranslate = document.querySelector("#btn-translate")
+console.log(btnTranslate);
+
+btnTranslate.addEventListener("click", function clickEventHandler(){
+    console.log("cclicked");
+})
