0f7d7b12a4187c37f298b8da836d38e5289f6648
Aman Tandon <aman.tandon3096@gmail.com>
2021-05-20 15:34:43 +0530

    output shown via input

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ console.log(inputEle);
 let btnTranslate = document.querySelector("#btn-translate")
 console.log(btnTranslate);
 
+let translatedText = document.querySelector("#translated-text");
+console.log(btnTranslate); 
 btnTranslate.addEventListener("click", function clickEventHandler(){
     console.log("cclicked");
+    translatedText.innerText = "translated: "+ inputEle.value;
 })
